4b811e439ada9cedef4b1db76d1cf9df115137d1
YuryRegis <yury_regis@hotmail.com>
2021-08-08 19:55:22 -0300

    User controller

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+
+    has_secure_password
+    
+    EMAIL_REGEX = /\A[^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$]\z/i
+    
+    validates :name, 
+        presence: true, 
+        length: {maximum: 50}
+
+    validates :password, 
+        length: {minimum: 6}
+
+    validates :email, 
+        presence: true, 
+        length: {maximum: 225},
+        format: {with: EMAIL_REGEX},
+        uniqueness: {case_sensitive: true}
+
+end
